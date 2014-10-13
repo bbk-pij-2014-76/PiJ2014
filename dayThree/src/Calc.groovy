e09bf45a0816dbd05b483cac8522bd532f274d41
Keith Mannock <keith@dcs.bbk.ac.uk>
2014-10-13 20:41:02 +0100

    Groovy dayThree

---
+++ dayThree/src/Calc.groovy
@@ -0,0 +1,7 @@
+            int op1 = Integer.parseInt(args[0])
+            String operator = args[1]
+            int op2 = Integer.parseInt(args[2])
+
+            if (operator.equals("+")) {
+                System.out.println( op1 + op2);
+            }
