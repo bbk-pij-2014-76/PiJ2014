590b948921e25cd391a74d036ef4a4f0fc0cf28b
Keith Mannock <keith@dcs.bbk.ac.uk>
2014-10-14 12:10:37 +0100

    FractionTest

---
+++ cw2spec/src/FractionTest.java
@@ -0,0 +1,20 @@
+/**
+ * Created by keith for the second coursework assignment.
+ */
+public class FractionTest {
+    public static void main(String[] args) {
+
+        // test divide by zero - should print an error and exit
+        new Fraction(1, 0);
+        // test multiply
+        assert ((new Fraction(3, 10)).equals(new Fraction(1, 2).multiply(new Fraction(3, 5)));
+        // test equals
+        assert (new Fraction(1, 2).equals(new Fraction(1, 2)));
+        assert (new Fraction(1, 2).equals(new Fraction(3, 6)));
+        assert (new Fraction(-1, 2).equals(new Fraction(1, -2)));
+        assert (new Fraction(-1, -2).equals(new Fraction(1, 2)));
+
+        // extend with extra tests
+    }
+
+}
